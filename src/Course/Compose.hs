dc375d43e227ae4443a1795b2bad5cd6e95cf4a9
Nathan Maxson <joyfulmantis@gmail.com>
2017-08-26 00:51:01 +0800

    added my Compose answers

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -8,6 +8,7 @@ import Course.Functor
 import Course.Applicative
 import Course.Monad
 
+import Course.List
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
 newtype Compose f g a =
@@ -16,20 +17,18 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  (<$>) func (Compose x) = Compose ((<$>) func <$> x)
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure x = Compose (pure (pure x))
+    
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  (<*>) (Compose func) (Compose app) = Compose $ lift2 (<*>) func app
+    
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+  (=<<)  = undefined
